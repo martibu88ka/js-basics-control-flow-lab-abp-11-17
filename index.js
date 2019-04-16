782ed741ad24ba49da23bb6df657e5be6306a704
Martyna <martyna.glaz@gmail.com>
2019-04-16 02:26:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride){
   if ((ride)<=400){
-    return 'This one is on me!'
+    return 'This one is on me!';
   }else if ((ride)>2000) {
-    return "I will gladly take your thirty bucks."
-
+    return "I will gladly take your thirty bucks.";
+  }else if ((ride)>2500) {
+    return "No can do.";
   }
 }
