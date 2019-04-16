6302eb0d331893847f49d9b0910f96e7b06a39ce
Martyna <martyna.glaz@gmail.com>
2019-04-16 03:00:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function ternaryCheckCity(city){
 function switchOnCharmFromTip(tip){
   if ((tip) === "generous"){
     return "Thank you so much."
-
   }else if ((tip) === "not as generous"){
     return "Thank you.";
   }else {
