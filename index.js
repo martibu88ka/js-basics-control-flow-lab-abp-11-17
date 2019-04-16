9daba8d635768b24d2eb80421fdebcb6bd4bc081
Martyna <martyna.glaz@gmail.com>
2019-04-16 03:01:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if ((ride)<=400){
+    return 'This one is on me!';
+  }else if ((ride)>2500) {
+    return "No can do.";
+  }else if ((ride)>2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+}
+function ternaryCheckCity(city){
+  if ((city)=="NYC"){
+    return "Ok, sounds good.";
+  }else {
+    return "No go.";
+  }
+}
+function switchOnCharmFromTip(tip){
+  if ((tip) === "generous"){
+    return "Thank you so much."
+  }else if ((tip) === "not as generous"){
+    return "Thank you.";
+  }else {
+    return "Bye."
+}
+}
