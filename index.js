9106646f6c4ce3e5de6dc7a0a5a0b307694a7190
Martyna <martyna.glaz@gmail.com>
2019-04-16 02:28:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 function scuberGreetingForFeet(ride){
   if ((ride)<=400){
     return 'This one is on me!';
+  }else if ((ride)>2500) {
+    return "No can do.";
   }else if ((ride)>2000) {
     return "I will gladly take your thirty bucks.";
-  }else  {
-    return "No can do.";
+
   }
 }
